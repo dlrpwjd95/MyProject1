60a1992afd6692749e46a9ae1d0316a764baeb28
LeeKyeJeong <dlrpwjd95@naver.com>
2023-03-16 14:16:01 +0900

    Commit 1 메인 함수 만듬. 더하기 함수 추가.

---
+++ calc.py
@@ -0,0 +1,13 @@
+## 함수(=메서드) 선언부
+def add_func(n1, n2) : 
+    res = n1 + n2
+    return res
+ 
+
+## 전역 변수부(필드,속성)
+num1, num2, result = 100, 200, 0
+
+## 메인 코드부: main() 메서드
+result = add_func(num1, num2)
+print(num1, '+', num2, '=', result)
+
